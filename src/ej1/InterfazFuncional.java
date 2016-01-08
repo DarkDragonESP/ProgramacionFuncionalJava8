009b2470a07fc46532522d919d0b2b9f368bb806
DarkDragonESP <fms.etsii@gmail.com>
2016-01-08 17:39:43 +0100

    ej6

--- src/ej1/InterfazFuncional.java
+++ src/ej1/InterfazFuncional.java
@@ -3,4 +3,12 @@ package ej1;
 @FunctionalInterface
 public interface InterfazFuncional{
 	public void sumar(int v1,int v2);
+	
+	public default int doble(int n) {
+        return 2*n;
+    }
+	
+	public default int triple(int n) {
+        return 3*n;
+    }
 }
---
+++ src/ej6/DevolverFuncion.java
@@ -0,0 +1,25 @@
+package ej6;
+
+import java.util.function.BiFunction;
+
+public class DevolverFuncion {
+	enum TipoEvaluacion {Continua, ExamenFinal};
+
+	public static BiFunction<Double, Double, Double> calcularNotaFinal(TipoEvaluacion tipo) {
+		switch (tipo) {
+		case Continua:
+			return (parcial, exFinal) -> parcial*0.2 + exFinal*0.8;
+		case ExamenFinal:
+			return (parcial,exFinal) -> exFinal;
+		default:
+			return null;
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		BiFunction<Double, Double, Double> variable = calcularNotaFinal(TipoEvaluacion.ExamenFinal);
+		System.out.println("Nota final: "+String.format("%.2f",  variable.apply(4.5,7.0)));
+
+	}
+}
